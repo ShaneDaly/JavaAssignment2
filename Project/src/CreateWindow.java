6754fdb2c913ea9445e4928680e76d5a03b8afff
Shnae <sdjdaly@live.com>
2015-11-17 11:05:37 +0000

    Edited the Main Window to have a sign up and sign in option. Created a sign in page.

--- Project/src/CreateWindow.java
+++ Project/src/CreateWindow.java
@@ -4,26 +4,47 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 
 public class CreateWindow extends JFrame implements ActionListener
 {
+	private JTextField u = new JTextField();
+	private JLabel username = new JLabel("Username:");
+	private JTextField p = new JTextField();
+	private JLabel password = new JLabel("Password:");
 	private JButton theOKButton = new JButton("Ok");
 	private JButton theCancelButton = new JButton("Cancel");
+    static String un;
+    static String pw;
 	
 	public CreateWindow()
 	{
-		super("Create Window");
+		super("SignUp");
 		getContentPane().setLayout(new GridLayout(3,1));
+		getContentPane().add(username);
+		getContentPane().add(password);
+		getContentPane().add(u);
+		getContentPane().add(p);
 		getContentPane().add(theOKButton);
 		getContentPane().add(theCancelButton);
 		
 		setSize(300,300);
 		setVisible(true);
+		
+		theOKButton.addActionListener(this);
+		theCancelButton.addActionListener(this);
 	}
 	
 	public void actionPerformed(ActionEvent e) 
 	{
+		if (e.getSource().equals(theOKButton)) 
+		{
+			un = u.getText();
+    		pw = p.getText();
+		}
+		
 		if (e.getSource().equals(theCancelButton)) 
 		{
 			dispose();
--- Project/src/MainWindow.java
+++ Project/src/MainWindow.java
@@ -7,44 +7,45 @@ import javax.swing.JFrame;
 
 public class MainWindow extends JFrame implements ActionListener
 {
-	private JButton theCreateWindowButton = new JButton("Create");
-	private JButton theReadWindowButton = new JButton("Read");
-	private JButton theUpdateWindowButton = new JButton("Update");
-	private JButton theDeleteWindowButton = new JButton("Delete");
+	private JButton theSignupButton = new JButton("Sign Up");
+	private JButton theSigninButton = new JButton("Sign In");
+	private JButton theUpdateWindowButton = new JButton("Update Account");
+	private JButton theDeleteWindowButton = new JButton("Delete Account");
 	
 	public MainWindow()
 	{
-		super("CRUD");
+		super("Moodle");
 		getContentPane().setLayout(new GridLayout(3,1));
-		getContentPane().add(theCreateWindowButton);
-		getContentPane().add(theReadWindowButton);
-		getContentPane().add(theUpdateWindowButton);
-		getContentPane().add(theDeleteWindowButton);
+		getContentPane().add(theSignupButton);
+		getContentPane().add(theSigninButton);
 		setSize(300,300);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		theCreateWindowButton.addActionListener(this);
+		theSignupButton.addActionListener(this);
+		theSigninButton.addActionListener(this);
 	}
 	
 	public void actionPerformed(ActionEvent e)
 	{
-		if(e.getSource().equals(theCreateWindowButton))
+		if(e.getSource().equals(theSignupButton))
 		{
 			new CreateWindow();
 		}
 		
-		else if(e.getSource().equals(theReadWindowButton)){
+		else if(e.getSource().equals(theSigninButton))
+		{
+			new SigninWindow();
+		}
+		
+		else if(e.getSource().equals(theUpdateWindowButton))
+		{
 				
 			
 		}
 		
-		else if(e.getSource().equals(theUpdateWindowButton)){
-				
-			
-		}
-		
-		else if(e.getSource().equals(theDeleteWindowButton)){
+		else if(e.getSource().equals(theDeleteWindowButton))
+		{
 			
 			
 		}
